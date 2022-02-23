1e379bbd19a81f8f67a1adbdadb1d5ccd9877e28
Moritz Wörle <moritz@woerle.dev>
2022-02-23 16:14:54 +0100

    Update config after upgrading to tailwind 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,15 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    mode: 'jit',
-    purge: {
-        enabled: true,
-        content: ['./src/index.html'],
-        options: {
-            keyframes: true,
-        },
-    },
-    darkMode: 'media',
+    content: [
+        './src/index.html'
+    ],
     theme: {
         extend: {
             fontFamily: {
                 sans: ['"Source Sans Pro"', ...defaultTheme.fontFamily.sans],
             },
         },
-    },
-    variants: {
-        extend: {},
     },
     plugins: [],
 }
