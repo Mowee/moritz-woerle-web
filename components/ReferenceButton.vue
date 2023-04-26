1be9c8d70c3419cb0ee9bea70b5bd2ddfdfc122a
Moritz Wörle <moritz@woerle.dev>
2023-04-26 10:46:31 +0200

    Use icon as dynamic component

--- components/ReferenceButton.vue
+++ components/ReferenceButton.vue
@@ -1,15 +1,19 @@
 <template>
     <a :href="url" class="dark:text-gray-900 dark:bg-gray-200 text-slate-100 bg-gray-800 rounded-full p-2 flex items-center">
-        <!-- TODO: Figure out why dynamic components aren't working! -->
-        <IconGitHub class="h-5 w-5 mr-2" v-if="icon === 'IconGitHub'" />
-        <IconXing class="h-5 w-5 mr-2" v-if="icon === 'IconXing'" />
-        <IconLinkedIn class="h-5 w-5 mr-2" v-if="icon === 'IconLinkedIn'" />
+        <Component :is="icon" class="h-5 w-5 mr-2" />
         {{ title }}
     </a>
 </template>
 
 <script lang="ts">
+import { IconGitHub, IconLinkedIn, IconXing } from "#components";
+
 export default {
+    components: {
+        IconGitHub,
+        IconLinkedIn,
+        IconXing,
+    },
     props: {
         title: {
             type: String,
